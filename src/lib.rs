89b48f699efcb612f2341bda2d0e744df762c1f6
Mikael Voss <mvs@nyantec.com>
2025-09-29 14:43:41 +0200

    Generate bindings during build
    This also changes the licence to LGPL 2.1 or later to match the
    upstream library licence.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,19 @@
-#![allow(non_camel_case_types,non_snake_case,non_upper_case_globals)]
+#![allow(non_camel_case_types, non_snake_case, non_upper_case_globals)]
 
-pub mod sensors;
-pub mod error;
+pub mod sensors {
+	include!(concat!(env!("OUT_DIR"), "/sensors.rs"));
+}
 
-pub use sensors::*;
-pub use error::*;
+pub mod error {
+	include!(concat!(env!("OUT_DIR"), "/error.rs"));
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn sensors_init() {
+		assert_eq!(unsafe { sensors::sensors_init(std::ptr::null_mut()) }, 0);
+	}
+}
