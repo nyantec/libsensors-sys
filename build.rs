acbce9aa235c6cb656741e4fc9cd4db461d67229
Mikael Voss <mvs@nyantec.com>
2025-09-29 14:43:41 +0200

    Generate bindings during build
    This also changes the licence to LGPL 2.1 or later to match the
    upstream library licence.

--- build.rs
+++ build.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    println!("cargo:rustc-link-lib=dylib=sensors");
+	let out = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+
+	for module in ["sensors", "error"] {
+		let mut path = out.join(module);
+		path.set_extension("rs");
+
+		bindgen::builder()
+			.header_contents("include.h", &format!("#include <sensors/{module}.h>"))
+			.allowlist_file(format!(r".*/sensors/{module}\.h"))
+			.default_enum_style(bindgen::EnumVariation::Rust { non_exhaustive: true })
+			.generate_cstr(true)
+			.generate()
+			.unwrap_or_else(|err| panic!("Unable to generate bindings for <sensors/{module}.h>: {err}"))
+			.write_to_file(&path)
+			.unwrap_or_else(|err| panic!("Failed to write bindings to {path:?}: {err}"));
+	}
 }
