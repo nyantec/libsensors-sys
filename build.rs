e418f08264fc97c35504bf685c70fb119c8550cd
Mikael Voss <mvs@nyantec.com>
2025-09-29 15:02:15 +0200

    Provide pre‐generated fallback bindings

--- build.rs
+++ build.rs
@@ -2,20 +2,43 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-	let out = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+	let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
+	let src_dir = {
+		let mut src_dir = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+		src_dir.push("src");
+		src_dir
+	};
 
 	for module in ["sensors", "error"] {
-		let mut path = out.join(module);
-		path.set_extension("rs");
+		let out = {
+			let mut out = out_dir.join(module);
+			out.set_extension("rs");
+			out
+		};
 
-		bindgen::builder()
+		match bindgen::builder()
 			.header_contents("include.h", &format!("#include <sensors/{module}.h>"))
 			.allowlist_file(format!(r".*/sensors/{module}\.h"))
 			.default_enum_style(bindgen::EnumVariation::Rust { non_exhaustive: true })
 			.generate_cstr(true)
 			.generate()
-			.unwrap_or_else(|err| panic!("Unable to generate bindings for <sensors/{module}.h>: {err}"))
-			.write_to_file(&path)
-			.unwrap_or_else(|err| panic!("Failed to write bindings to {path:?}: {err}"));
+		{
+			Ok(bindings) => bindings
+				.write_to_file(&out)
+				.unwrap_or_else(|err| panic!("Failed to write bindings to {out:?}: {err}")),
+
+			Err(err) => {
+				eprintln!("Unable to re‐generate bindings for <sensors/{module}.h>: {err}");
+
+				// Fall back to pre‐generated bindings
+				let src = {
+					let mut src = src_dir.join(module);
+					src.set_extension("rs");
+					src
+				};
+
+				std::fs::copy(&src, &out).unwrap_or_else(|err| panic!("Failed to copy {src:?} to {out:?}: {err}"));
+			}
+		}
 	}
 }
